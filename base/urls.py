c02d72a3e42e2d02cc26f99617f906863ef5a793
PurityKomen <jepkorirpurity8@gmail.com>
2025-03-25 09:53:07 +0300

    added a form to create and update rooms

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name='Home'),
     path('room/<str:pk>/',views.room, name='Room'),
+    path('create-room/',views.createRoom, name='create-room'),
 ]
